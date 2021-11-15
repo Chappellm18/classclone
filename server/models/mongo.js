2d0eb2ee14a0dad8d73e048a52f6f6600131b5b5
Chappellm <Chappelm1@hawkmail.newpaltz.edu>
2021-11-15 14:19:02 -0500

    database isnt working

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,9 +1,9 @@
 /*  B"H
 */
-
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ooi3z.mongodb.net/CLassClone?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://<${process.env.MONGO_USER}:${process.env.MONGO_PASS}@workoutcluster.xuajk.mongodb.net/AppUsers?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
